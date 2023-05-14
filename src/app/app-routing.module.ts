9a43cd83760ea12f4b1030f969eac8c59143afc2
mukesh <mukeshvancha122@gmail.com>
2023-05-15 04:04:10 +0530

    Initial commmit and navbar

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: 'seller-auth',
+    component: SellerAuthComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
