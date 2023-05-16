4abb41c7be7d638d08458d0ed696717b85c546e2
mukesh <mukeshvancha122@gmail.com>
2023-05-17 00:15:14 +0530

    Initial commit and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 import { HomeComponent } from './home/home.component';
+import { SellerHomeComponent } from './seller-home/seller-home.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'seller-home',
+    component: SellerHomeComponent,
   },
 ];
 
--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component ,OnInit} from '@angular/core';
+import { SellerService } from '../services/seller.service';
+import { signUp } from '../services/data-types';
+import {Router} from "@angular/router";
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
   styleUrls: ['./seller-auth.component.css']
 })
-export class SellerAuthComponent {
+export class SellerAuthComponent implements OnInit {
+
+  constructor(private seller:SellerService, private router:Router){
+
+  }
+  ngOnInit(): void {
+
+  }
+  signUp(data:signUp){
+    console.log(data)
+    this.seller.userSignUp(data).subscribe((result)=>{
+      console.warn(result)
+      if(result){
+        this.router.navigate(['seller-home'])
+      }
+    })
+    }
+
 
 }
